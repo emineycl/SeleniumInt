510286ba70d7d275168119f717205f3d8e88347b
Emine Yucel <emn.yl.06@gmail.com>
2022-10-14 21:13:08 +0200

    first commit

--- src/test/java/practice/Q5.java
+++ src/test/java/practice/Q5.java
@@ -1,5 +1,13 @@
 package practice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Q5 {
     // .......Exercise5........
     /*
@@ -15,7 +23,42 @@ public class Q5 {
     and then verify Submitted Values is displayed open page
     close driver
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+        Thread.sleep(1000);
+
+     driver.findElement(By.xpath("//a[@id='alerts']")).click();
+        System.out.println(driver.getCurrentUrl());
+        Thread.sleep(1000);
+
+        driver.navigate().back();
+        System.out.println(driver.getCurrentUrl());
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//a[@id='basicajax']")).click();
+        System.out.println(driver.getCurrentUrl());
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("20"+ Keys.ENTER);
+        Thread.sleep(1000);
+
+        String expectedText="Submitted Values";
+        String actualText=driver.findElement(By.xpath("//p[text()='Submitted Values']")).getText();
+        Thread.sleep(1000);
+
+        if (expectedText.equals(actualText)) System.out.println("Test Passed");
+        else System.out.println("Test Failed");
+
+
+        driver.close();
+
+
+
 
     }
 }
