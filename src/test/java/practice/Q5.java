fe19b750b22e42fe59f0931d799e1e60dcb93849
Emine Yucel <emn.yl.06@gmail.com>
2022-10-14 20:33:59 +0200

    first commit

---
+++ src/test/java/practice/Q5.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Q5 {
+    // .......Exercise5........
+    /*
+    Navigate to website https://testpages.herokuapp.com/styled/index.html
+    Under the ORIGINAL CONTENTS
+    click on Alerts
+    print the URL
+    navigate back
+    print the URL
+    Click on Basic Ajax
+    print the URL
+    enter value 20 and ENTER
+    and then verify Submitted Values is displayed open page
+    close driver
+     */
+    public static void main(String[] args) {
+
+    }
+}
