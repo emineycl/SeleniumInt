820035f821d8fedb8fba0a17939c8c225d16660a
Emine Yucel <emn.yl.06@gmail.com>
2022-10-14 19:52:09 +0200

    first commit

--- src/test/java/practice/Q3.java
+++ src/test/java/practice/Q3.java
@@ -1,12 +1,15 @@
 package practice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 public class Q3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         /* ...Exercise3...
     go to url : https://www.techlistic.com/p/selenium-practice-form.html
@@ -24,12 +27,43 @@ public class Q3 {
 
         System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-
          driver.manage().window().maximize();
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));  //implicitlyWait seleniumdan geliyor
+        driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
 
-         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().deleteAllCookies();
 
-        driver.get("https://www.otto.de");
+        driver.findElement(By.xpath("//button[@id='ez-accept-all']")).click();  //cikan pencereyi acmak icin
+
+        driver.findElement(By.xpath("//a[@id='cookieChoiceDismiss']")).click();  // --> //a[@id='   ']
+
+        driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Nevzat"+ Keys.ENTER);
+        Thread.sleep(1000);  //java dan geliyor*
+
+        driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Celik" + Keys.ENTER);
+        Thread.sleep(1000);            // "//input{[@name='  '] "
+
+        WebElement genderRadioButton = driver.findElement(By.xpath("//input[@id='sex-0']"));  //Gender
+       genderRadioButton.click();
+        Thread.sleep(1000);
+        genderRadioButton.sendKeys(Keys.PAGE_DOWN);
+
+        driver.findElement(By.xpath("//input[@id='exp-2']")).click();
+        Thread.sleep(1000);
+      driver.findElement(By.xpath("//input[@id='datepicker']")).sendKeys("12.02.2023"+ Keys.ENTER);
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//input[@id='profession-1']")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//input[@id='tool-2']")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//select[@class='input-xlarge']")).sendKeys("Europa" +Keys.ENTER + Keys.ENTER);
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//select[@id='selenium_commands']")).sendKeys("Browser Commands" +Keys.ENTER);
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//button[@class='btn btn-info']")).click();
+      Thread.sleep(1000);
+
+      driver.close();
 
 
     }
