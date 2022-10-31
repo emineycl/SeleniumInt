1dab8f8ed817ec0d4a77cc87f799df468c125039
Emine Yucel <emn.yl.06@gmail.com>
2022-10-31 20:52:44 +0100

    com.testNG

---
+++ JUnitInt/src/test/java/_AutomationExircise_Aurora/TestCase2.java
@@ -0,0 +1,2 @@
+package _AutomationExircise_Aurora;public class TestCase2 {
+}
---
+++ com.testNgPOM_Fall2022/src/test/java/utilities/TestBase.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class TestBase {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+}
